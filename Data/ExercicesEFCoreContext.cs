feb47ea4c81d817558029f68ef84527b0ee0b8dd
Christoooophe <christophealfier02@gmail.com>
2025-01-14 14:13:43 +0100

    Ajoutez des fichiers projet.

---
+++ Data/ExercicesEFCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ExercicesEFCore.Models;
+
+namespace ExercicesEFCore.Data
+{
+    public class ExercicesEFCoreContext : DbContext
+    {
+        public ExercicesEFCoreContext (DbContextOptions<ExercicesEFCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExercicesEFCore.Models.ToDo> ToDo { get; set; } = default!;
+    }
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace ExercicesEFCore.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        [DisplayName("Titre")]
+        public string Title { get; set; }
+        [DisplayName("Description")]
+        public string Description { get; set; }
+        [DisplayName("Actions")]
+        public bool IsDone { get; set; }
+    }
+}
