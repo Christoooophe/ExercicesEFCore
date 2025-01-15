8ac46ebf33da459c1f3498d947757ae0fc00e7d0
Christoooophe <christophealfier02@gmail.com>
2025-01-15 14:54:02 +0100

    Ajout seeder

---
+++ Models/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using ExercicesEFCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExercicesEFCore.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ExercicesEFCoreContext(serviceProvider.GetRequiredService<DbContextOptions<ExercicesEFCoreContext>>()))
+            {
+                //if (context.ToDo.Any())
+                //{
+                //    return;
+                //}
+                context.ToDo.AddRange(
+                    new ToDo
+                    {
+                        Title = "Teeeeeeest1",
+                        Description = "Teeeeeeeeeeeeeeeeeeeest 1 description",
+                        IsDone = true,
+                    },
+                    new ToDo
+                    {
+                        Title = "Teeeeeest2",
+                        Description = "Teeeeeeeeeeeeeeeeeeest 2 description",
+                        IsDone = false,
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ExercicesEFCore.Data;
+using ExercicesEFCore.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ExercicesEFCoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ExercicesEFCoreContext") ?? throw new InvalidOperationException("Connection string 'ExercicesEFCoreContext' not found.")));
@@ -10,6 +11,12 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
